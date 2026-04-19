da0043ea6f0964dac775b11e95ffa8d90d3d4638
youandmeonthelandline <nononoanhdethuongquamatthoi@gmail.com>
2026-04-19 17:46:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ bgMusic.volume = 0.45;
 
 const playlist = [
   {src:"asset/phokhongem.mp3", name:"🎶 Phố không em"},
-  {src:"asset/song1.mp3", name:"💗 Bài 1"},
+  {src:"asset/noinaycoanh.mp3", name:"💗 Nơi này có anh"},
   {src:"asset/song2.mp3", name:"🌙 Bài 2"},
   {src:"asset/song3.mp3", name:"✨ Bài 3"}
 ];
